ad48718cfcb0c0e18fdf45f3c5b85c4769774de6
Philip Kealley <pkealley@gmail.com>
2018-03-08 20:38:34 +0800

    General case, Any size shape

--- Week1-2/exercise3.c
+++ Week1-2/exercise3.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef float Point3[3];
-typedef int Face[3];
+typedef struct Point3{
+    float x, y, z; 
+ } Point3;
+
+typedef  struct Face{
+    int nvert;
+    int *index;
+} Face;
 
 typedef struct Object3D{
     int nvert, nfaces, nedges;
-    Point3 *verticies;
-    Face *faces;
+    Point3 *list_verticies;
+    Face *list_faces;
 } Object3D;
 
 void readOFFFile(const char *fname, Object3D *shape ){
@@ -17,17 +23,22 @@ void readOFFFile(const char *fname, Object3D *shape ){
     FILE * fp;
     fp = fopen (fname,"r"); //open file for reading
     fscanf (fp, "%s %d %d %d", str, &shape->nvert, &shape->nfaces, &shape->nedges);  //scan index values into struct
-    shape->verticies = (Point3*)malloc(shape->nvert * sizeof(Point3));  //initialise array of vectors
+    shape->list_verticies = malloc(shape->nvert * sizeof(Point3));  //initialise array of vectors
 
     for (i=0; i<shape->nvert; i++){  //increment through point list in OFF file.
-        fscanf(fp,"%f %f %f", &shape->verticies[i][0], &shape->verticies[i][1], &shape->verticies[i][2]); //scan in vectors into array
+        fscanf(fp,"%f %f %f", &shape->list_verticies[i].x, &shape->list_verticies[i].y, &shape->list_verticies[i].z); //scan in vectors into array
     }
 
-    shape->faces = (Face*)malloc(shape->nfaces * sizeof(Face)); //initialise array of nfaces
+    shape->list_faces = (Face*)malloc(shape->nfaces * sizeof(Face)); //initialise array of nfaces
 
     for(i=0; i<shape->nfaces; i++){   //increment through nfaces list in off file
-        fscanf(fp,"%d %d %d %d", &n, &shape->faces[i][0], &shape->faces[i][1], &shape->faces[i][2]);   //scan number of verticies in this face
+        fscanf(fp, "%d ", &shape->list_faces[i].nvert);
+        shape->list_faces[i].index = malloc(shape->list_faces[i].nvert * sizeof(int));
+        
+        for(n=0; n<shape->list_faces[i].nvert; n++){
+             fscanf(fp,"%d ", &shape->list_faces[i].index[n]);
         }
+    }
 
     fclose(fp); //close file
 
@@ -35,17 +46,23 @@ void readOFFFile(const char *fname, Object3D *shape ){
 
 void writeOFFFile (char fname[25], const Object3D *shape){
     FILE * fp;
-    int i;
+    int i, n;
 
     fp = fopen (fname,"w"); //open file for writing
     fprintf(fp, "OFF\n");
     fprintf(fp, "%d %d %d\n", shape->nvert, shape->nfaces, shape->nedges); //write counts
     for (i = 0; i<shape->nvert; i++){
-        fprintf(fp, "%f %f %f \n", shape->verticies[i][0], shape->verticies[i][1], shape->verticies[i][2]);  //write points
+        fprintf(fp, "%f %f %f \n", shape->list_verticies[i].x, shape->list_verticies[i].y, shape->list_verticies[i].z);  //write points
     }
     for (i = 0; i< shape->nfaces; i++){
-     fprintf(fp, "%d %d %d %d\n", 3 , shape->faces[i][0], shape->faces[i][1], shape->faces[i][2]);  //write faces
+        fprintf(fp, "%d ", shape->list_faces[i].nvert );
+    
+        for(n=0; n< shape->list_faces[i].nvert; n++){
+            fprintf(fp, "%d ", shape->list_faces[i].index[n]);  //write list_faces
+        }
+        fprintf(fp, "\n");
     }
+
     fclose(fp);
 }
 
