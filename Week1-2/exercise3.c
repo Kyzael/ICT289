5cc4331cc53928ad2a469c88f36d7bb8c1c631a6
Philip Kealley <pkealley@gmail.com>
2018-03-15 08:41:22 +0800

    Completed

--- Week1-2/exercise3.c
+++ Week1-2/exercise3.c
@@ -22,14 +22,14 @@ void readOFFFile(const char *fname, Object3D *shape ){
 
     FILE * fp;
     fp = fopen (fname,"r"); 
-    fscanf (fp, "%s %d %d %d", str, &shape->nvert, &shape->nfaces, &shape->nedges);  //Read number of verticies and faces in the OFF file. Change this to read all strings before the first int.
-    shape->list_verticies = malloc(shape->nvert * sizeof(Point3));  //initialise the array of verticies. (nvert * Point3)
+    fscanf (fp, "%s %d %d %d", str, &shape->nvert, &shape->nfaces, &shape->nedges);  //Read number of verticies and faces in the OFF file. |Change this to read all strings before the first int.
+    shape->list_verticies = malloc(shape->nvert * sizeof(Point3));  //initialise size of the array of verticies.
 
     for (i=0; i<shape->nvert; i++){  //increment through list of verticies in the file.
-        fscanf(fp,"%f %f %f", &shape->list_verticies[i].x, &shape->list_verticies[i].y, &shape->list_verticies[i].z); //read points into array of structs.
+        fscanf(fp,"%f %f %f", &shape->list_verticies[i].x, &shape->list_verticies[i].y, &shape->list_verticies[i].z); //read points into array of verticies.
     }
 
-    shape->list_faces = (Face*)malloc(shape->nfaces * sizeof(Face)); //initialise array of face. (nfaces * Face)
+    shape->list_faces = (Face*)malloc(shape->nfaces * sizeof(Face)); //initialise size of the array of faces.
     
     for(i=0; i<shape->nfaces; i++){   //increment through list of faces in the file.
         fscanf(fp, "%d ", &shape->list_faces[i].nvert);  // Red in the number indexes the face is commprised of
@@ -43,7 +43,7 @@ void readOFFFile(const char *fname, Object3D *shape ){
     fclose(fp); //close file
 }
 
-void writeOFFFile (char fname[25], const Object3D *shape){
+void writeOFFFile (const char *fname, const Object3D *shape){
     FILE * fp;
     int i, n;
 
@@ -65,15 +65,42 @@ void writeOFFFile (char fname[25], const Object3D *shape){
     fclose(fp);
 }
 
+void printVerticies(const Object3D *shape){
+    int i;
+
+    for (i = 0; i<shape->nvert; i++){
+        printf("%f %f %f \n", shape->list_verticies[i].x, shape->list_verticies[i].y, shape->list_verticies[i].z);  //print verticies list
+    }
+
+}
+
+void printTriangles(const Object3D *shape){
+    int i,n;
+
+    for (i = 0; i< shape->nfaces; i++){    
+        for(n=0; n< shape->list_faces[i].nvert; n++){
+            printf("%d ", shape->list_faces[i].index[n]);  //write face list
+        }
+        printf( "\n");
+    }
+}
+
 
 int main(){
 
-    int i;
     char fname[25] = "bone.off";
     char wname[25] = "new.off";
+    int i;
 
     Object3D shape;
     readOFFFile(fname, &shape);
-    writeOFFFile(wname, &shape);
+    //writeOFFFile(wname, &shape);
+
+    printf("Verticies:%d Faces:%d\n", shape.nvert, shape.nfaces);
+
+    getchar();
+    printVerticies(&shape);
+    getchar();
+    printTriangles(&shape);
 
 }
